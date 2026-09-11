37301bb5b4a489d7697d380371f7bcd42e78d86b
1U24IT017-BHARATH <bharathraja815@gmail.com>
2026-09-11 12:05:58 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,20 +1,29 @@
+```bash
 #!/bin/bash
 
 # =====================================
 # Swap Space Creation Script
-# Student Name:ABINAYA S
-# Roll Number:1U24IT002
+# Student Name: R Bharath
+# Roll Number: 1U24IT017
 # =====================================
-# Write your commands below
+
+# Create a 1 GB swap file
 sudo fallocate -l 1G /swapfile
+
+# Set proper permissions
 sudo chmod 600 /swapfile
+
+# Create swap area
 sudo mkswap /swapfile
+
+# Enable swap
 sudo swapon /swapfile
+
+# Display active swap space
 sudo swapon --show
+
+# Display memory and swap usage
 free -h
 
-
 exit 0
-
-
-
+```
